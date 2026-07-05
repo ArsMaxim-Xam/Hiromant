682d78fa7873f62c03cd981dc31c8fce70317070
ArsMaxim <arsmaxim@gmail.com>
2026-07-05 20:49:38 +0300

    Update PalmistDatabase.kt

--- app/src/main/java/com/aistudio/hiromant/kxsrwa/data/local/PalmistDatabase.kt
+++ app/src/main/java/com/aistudio/hiromant/kxsrwa/data/local/PalmistDatabase.kt
@@ -1,6 +1,8 @@
 package com.aistudio.hiromant.kxsrwa.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(
@@ -14,4 +16,21 @@ import androidx.room.RoomDatabase
 )
 abstract class PalmistDatabase : RoomDatabase() {
     abstract fun palmistDao(): PalmistDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: PalmistDatabase? = null
+
+        fun getDatabase(context: Context): PalmistDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PalmistDatabase::class.java,
+                    "palmist_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
