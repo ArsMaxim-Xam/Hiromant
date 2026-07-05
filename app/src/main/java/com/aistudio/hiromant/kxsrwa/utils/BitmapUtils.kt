2a99df3330fd9941ff96e396148b8f7e2b1391c9
ArsMaxim <arsmaxim@gmail.com>
2026-07-05 23:13:57 +0300

    Add files via upload

---
+++ app/src/main/java/com/aistudio/hiromant/kxsrwa/utils/BitmapUtils.kt
@@ -0,0 +1,27 @@
+package com.aistudio.hiromant.kxsrwa.utils
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.util.Base64
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+
+object BitmapUtils {
+    fun Bitmap.toBase64(quality: Int = 70): String {
+        val outputStream = ByteArrayOutputStream()
+        this.compress(Bitmap.CompressFormat.JPEG, quality, outputStream)
+        return Base64.encodeToString(outputStream.toByteArray(), Base64.NO_WRAP)
+    }
+
+    fun uriToBitmap(context: Context, uri: Uri): Bitmap? {
+        return try {
+            val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
+            BitmapFactory.decodeStream(inputStream)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+}
